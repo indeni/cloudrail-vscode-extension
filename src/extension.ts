f0a1129867b1dfce2e21d07137ffabb6c8315e4e
Tomer Vainshtok <tomerv@indeni.com>
2021-10-13 15:02:04 +0300

    Iniitializing environment on activate

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,12 @@ import { cloudrailVersion } from './commands/version';
 import { scan } from './commands/scan';
 import { initializeEnvironment } from './commands/init';
 
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cloudrail-iac-scanning" is now active!');
-	
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	initializeEnvironment();
 
 	const commands = [
 		vscode.commands.registerCommand('cloudrail.version', () => {
