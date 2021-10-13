8a931956d2e99de9bb00166653e722fcfaafd36d
Tomer Vainshtok <tomerv@indeni.com>
2021-10-13 11:25:59 +0300

    Initial structure

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { cloudrailVersion } from './commands/version';
+import { scan } from './commands/scan';
+import { initializeEnvironment } from './commands/init';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,17 +12,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cloudrail-iac-scanning" is now active!');
-
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cloudrail-iac-scanning.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Cloudrail IaC Scanning!');
-	});
 
-	context.subscriptions.push(disposable);
+	const commands = [
+		vscode.commands.registerCommand('cloudrail.version', () => {
+			cloudrailVersion();
+		}),
+
+		vscode.commands.registerCommand('cloudrail.scan', () => {
+			scan();
+		}),
+
+		vscode.commands.registerCommand('cloudrail.settings', () => {
+			vscode.commands.executeCommand('workbench.action.openSettings', 'cloudrail');
+		}),
+
+		vscode.commands.registerCommand('cloudrail.init', () => {
+			initializeEnvironment();
+		})
+	];
+
+	for (let command of commands) {
+		context.subscriptions.push(command);
+	}
 }
 
 // this method is called when your extension is deactivated
