030054923bd0edb3dc02db6fbbdd26bcd7cef497
Tomer Vainshtok <tomerv@indeni.com>
2021-10-14 09:57:31 +0300

    Added version dialog

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,3 +1,6 @@
-export function initializeEnvironment(): void {
+import { Versioning } from "../tools/versioning";
 
+export function initializeEnvironment(): void {
+    // To be set dynamically after installing cloudrail
+    Versioning.setCloudrailVersion('cloudrail, version 1.3.606\na Newer version ...');
 }
--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,18 +1,8 @@
-import { exec, ExecException } from 'child_process';
 import * as vscode from 'vscode';
+import { Versioning } from '../tools/versioning';
 
-export function cloudrailVersion(): void {
+export function cloudrailVersion(): void {    
+    const versionMsg = `Cloudrail Version: ${Versioning.getCloudrailVersion()}` + '\n' + `Extension Version: ${Versioning.getExtensionVersion()}`;
 
-    exec('cloudrail --version', (error: ExecException | null, stdout: string, stderr: string) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-        const extensionVersion = vscode.extensions.getExtension("Cloudrail.cloudrail-iac-scanning")?.packageJSON['version'];
-        vscode.window.showInformationMessage(`${stdout}\nextension, version ${extensionVersion}`, {modal:true});
-    });
-    
-    
+    vscode.window.showInformationMessage(versionMsg, {modal:true});
 }
